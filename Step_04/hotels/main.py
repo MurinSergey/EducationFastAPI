fabef4a573da71f69e1a05265d7f5563374ad50c
Murin Sergey <murin_sergey@vk.com>
2025-03-25 19:02:58 +0300

    Получение списка отолей по заданным параметрам или без них

--- Step_04/hotels/main.py
+++ Step_04/hotels/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import uvicorn
 
 # Создаем приложение FastAPI
@@ -13,13 +13,23 @@ hotels = [
 # Стартовая страница
 @app.get("/")
 def start():
-    return "Список отелей"
+    return hotels
 
 # Получаем список всех отелей
 @app.get("/hotels")
-def get_hotels():
-    return hotels
+def get_hotels(
+        id: int | None = Query(description="Номер отеля", default=None),
+        title: str | None = Query(description="Название отеля", default=None)
+    ):
+    _hotels = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        _hotels.append(hotel)
+    return _hotels
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8001)

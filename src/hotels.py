60dd41a22fd2216e8f2d86abce0c18c88b4ac6b6
Murin Sergey <murin.sergey@gmail.com>
2025-04-01 19:24:07 +0300

    В модуля hotels.py добавил схему Hotel

--- src/hotels.py
+++ src/hotels.py
@@ -1,4 +1,5 @@
 from fastapi import Body, Query, APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
@@ -9,6 +10,8 @@ hotels = [
 ]
 
 # Получаем список всех отелей
+
+
 @router.get("", summary="Список всех отелей")
 def get_hotels(
         id: int | None = Query(description="Номер отеля", default=None),
@@ -24,24 +27,44 @@ def get_hotels(
     return _hotels
 
 # Удаление выбранного отеля
+
+
 @router.delete("/{hotel_id}", summary="Удаление отеля")
 def delete_hotel(
         hotel_id: int,
-        hotel_titel: str | None = Query(description="Название отеля", default=None) # В метод delete можно добавлять параметры фильтрации
+        # В метод delete можно добавлять параметры фильтрации
+        hotel_titel: str | None = Query(
+            description="Название отеля", default=None)
     ):
     global hotels
     hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
     return {"status": "OK"}
 
+
+class Hotel(BaseModel):
+    """
+    Класс Hotel представляет отель.
+
+    Атрибуты:
+    title (str): Название отеля.
+    name (str): Имя отеля.
+    """
+    title: str
+    name: str
+
+
 # Создание нового отеля
 @router.post("", summary="Создание нового отеля")
 def create_hotel(
-        hotel_title: str = Body(description="Название отеля", embed=True),
-        hotel_name: str = Body(description="Код отеля", embed=True)
+        hotel_data: Hotel
     ):
 
     global hotels
-    hotels.append({"id": hotels[-1]["id"] + 1, "title": hotel_title, "name": hotel_name})
+    hotels.append({
+        "id": hotels[-1]["id"] + 1, 
+        "title": hotel_data.hotel_title, 
+        "name": hotel_data.hotel_name
+        })
 
     return {"status": "OK"}
 
@@ -49,14 +72,13 @@ def create_hotel(
 @router.put("/{hotel_id}", summary="Полное обновление данных")
 def replace_hotel(
         hotel_id: int,
-        hotel_title: str = Body(description="Название отеля", embed=True),
-        hotel_name: str = Body(description="Код отеля", embed=True)
+        hotel_data: Hotel
     ):
 
     for hotel in hotels:
         if hotel["id"] == hotel_id:
-            hotel["title"] = hotel_title
-            hotel["name"] = hotel_name
+            hotel["title"] = hotel_data.hotel_title
+            hotel["name"] = hotel_data.hotel_name
             break
 
     return {"status": "OK"}
@@ -65,15 +87,14 @@ def replace_hotel(
 @router.patch("/{hotel_id}", summary="Частичное обновление данных")
 def update_hotel(
         hotel_id: int,
-        hotel_title: str | None = Body(default=None, description="Название отеля", embed=True),
-        hotel_name: str | None = Body(default=None, description="Код отеля", embed=True)
+        hotel_data: Hotel
     ):
     for hotel in hotels:
         if hotel["id"] == hotel_id:
-            if hotel_title:
-                hotel["title"] = hotel_title
-            if hotel_name:
-                hotel["name"] = hotel_name
+            if hotel_data.hotel_title:
+                hotel["title"] = hotel_data.hotel_title
+            if hotel_data.hotel_name:
+                hotel["name"] = hotel_data.hotel_name
             break
 
     return {"status": "OK"}
--- src/main.py
+++ src/main.py
@@ -9,4 +9,4 @@ app = FastAPI()
 app.include_router(hotels_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8001)
+    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8000)

399d03040481327926a581875ba2f0bae07bc0ae
Murin Sergey <murin.sergey@gmail.com>
2025-04-16 22:21:41 +0300

    Добавил фильтрацию по названию и адресу, используя частичное совпадение строки через ILIKE

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -12,16 +12,17 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 @router.get("", summary="Список всех отелей")
 async def get_hotels(
     pagination: PaginationDep,
-    id: int | None = Query(description="Номер отеля", default=None),
     title: str | None = Query(description="Название отеля", default=None),
+    location: str | None = Query(description="Адрес отеля", default=None),
 ):
-    per_page = pagination.per_page or 5 # По умолчанию выводим 5 отелей на странице
+    per_page = pagination.per_page or 5  # По умолчанию выводим 5 отелей на странице
     async with async_session_maker() as session:
         query = select(HotelsOrm)
-        if id:
-            query = query.filter(id=id)
+
         if title:
-            query = query.filter(title=title)
+            query = query.filter(HotelsOrm.title.ilike(f"%{title}%"))
+        if location:
+            query = query.filter(HotelsOrm.location.ilike(f"%{location}%"))
 
         query = (
             query
@@ -30,6 +31,8 @@ async def get_hotels(
         )
         result = await session.execute(query)
         hotels = result.scalars().all()
+        # print(query.compile(bind=engine,
+        #       compile_kwargs={"literal_binds": True}))
         return hotels
 
 # Удаление выбранного отеля
@@ -43,7 +46,6 @@ def delete_hotel(
     global hotels
     hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
     return {"status": "OK"}
-
 
 # Создание нового отеля
 @router.post("", summary="Создание нового отеля")
@@ -67,7 +69,8 @@ async def create_hotel(
 ):
 
     async with async_session_maker() as session:
-        add_hotel_statement = insert(HotelsOrm).values(**hotel_data.model_dump())
+        add_hotel_statement = insert(HotelsOrm).values(
+            **hotel_data.model_dump())
         # print(add_hotel_statement.compile(bind=engine, compile_kwargs={"literal_binds": True}))
         await session.execute(add_hotel_statement)
         await session.commit()
@@ -75,8 +78,6 @@ async def create_hotel(
     return {"status": "OK"}
 
 # Изменение всего объекта
-
-
 @router.put("/{hotel_id}", summary="Полное обновление данных")
 def replace_hotel(
     hotel_id: int,
@@ -92,8 +93,6 @@ def replace_hotel(
     return {"status": "OK"}
 
 # Изменение части объекта
-
-
 @router.patch("/{hotel_id}", summary="Частичное обновление данных")
 def update_hotel(
     hotel_id: int,

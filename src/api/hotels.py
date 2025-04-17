82d6f6c8a17f6e2838c043244721e5042a766cb2
Murin Sergey <murin_sergey@vk.com>
2025-04-17 15:30:36 +0300

    Использование contains вместо like

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -24,14 +24,20 @@ async def get_hotels(
             # query = query.filter(HotelsOrm.title.ilike(f"%{title}%")) 
 
             # Используем func.lower для преобразования к нижнему регистру и поиск по подстроке без учета регистра
-            query = query.filter(func.lower(HotelsOrm.title).like(f"%{title.lower().strip()}%"))
+            # query = query.filter(func.lower(HotelsOrm.title).like(f"%{title.lower().strip()}%"))
+
+            # Используем contains для поиска по подстроке без учета регистра вместо like или ilike, как более безопасный вариант
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.lower().strip()))
 
         if location:
             # Используем ilike для поиска по подстроке без учета регистра
             # query = query.filter(HotelsOrm.location.ilike(f"%{location}%"))
 
             # Используем func.lower для преобразования к нижнему регистру и поиск по подстроке без учета регистра
-            query = query.filter(func.lower(HotelsOrm.location).like(f"%{location.lower().strip()}%"))
+            # query = query.filter(func.lower(HotelsOrm.location).like(f"%{location.lower().strip()}%"))
+
+            # Используем contains для поиска по подстроке без учета регистра вместо like или ilike, как более безопасный вариант
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.lower().strip()))
 
         query = (
             query
@@ -40,8 +46,7 @@ async def get_hotels(
         )
         result = await session.execute(query)
         hotels = result.scalars().all()
-        # print(query.compile(bind=engine,
-        #       compile_kwargs={"literal_binds": True}))
+        # print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
         return hotels
 
 # Удаление выбранного отеля
